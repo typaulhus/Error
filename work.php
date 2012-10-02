033490242534f5d6026e7776b5da06833fb2b444
Ty Paulhus <typaulhus@gmail.com>
2012-10-02 16:27:31 -0400

    gDrive MAMP installed theme

---
+++ work.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Work
+*/
+?>
+
+<?php get_header(); ?>
+  <div class="workMain">
+  	<ul class="g">
+			<li><a href="<?php bloginfo('template_directory'); ?>/images/fpolarge.jpg" rel="lightbox"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+			<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/fpo.jpg" rel="lightbox" alt="Product Name" /></a></li>
+
+
+		</ul>
+	</div>
+<?php get_footer(); ?>
