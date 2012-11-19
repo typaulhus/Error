af96fe6567888c9736f9d3f7c559c486168df981
Ty Paulhus <typaulhus@gmail.com>
2012-11-18 22:23:02 -0500

    about updates

--- sidebarabout.php
+++ sidebarabout.php
@@ -10,6 +10,6 @@
 
 	<aside class="sidebarabout">
 	
-	<h3>About</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor.</p>	
+<!-- 	<h3>Random Factoid:</h3>
+		 -->		<img src="<?php bloginfo('template_directory'); ?>/images/tyboys.jpg">	
 	</aside>
