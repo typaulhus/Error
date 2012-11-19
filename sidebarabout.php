d979227ae49be7de0ce7f1fb9abfac8161c278ec
Ty Paulhus <typaulhus@gmail.com>
2012-11-18 23:21:23 -0500

    about updates

--- sidebarabout.php
+++ sidebarabout.php
@@ -11,5 +11,5 @@
 	<aside class="sidebarabout">
 	
 <!-- 	<h3>Random Factoid:</h3>
-		 -->		<img src="<?php bloginfo('template_directory'); ?>/images/tyboys.jpg">	
+		 -->		<img src="<?php bloginfo('template_directory'); ?>/images/tyboys.jpg" title="Me. sans-beard">	
 	</aside>
