e472bdfac03ca7a8a3069f0cacce6b04bb7145d8
Ty Paulhus <typaulhus@gmail.com>
2012-11-21 17:11:36 -0500

    about update

--- about.php
+++ about.php
@@ -8,15 +8,9 @@ Template Name: About
   
   
   <div class="aboutMain">
-	<!-- <p><span class="hilight">Where to begin.</span></p> -->
-	<!-- <p>
-	I'm not sure I even know where to begin. I've been a painter and an illustrator as far back as I can remember. I've been around design my entire life, both my dad & grandfather are designers, Some of my earliest memories are of playing in my dad's studio, handcut page mockups, trips to the typesetter, and drawing on top of proof sheets. This was obviously a huge influence. Next, in no particular order, are skateboarding, music, and graffiti. I couldn't possibly know where to begin in how these things shaped who I am today, and I wont even attempt it. You've got the basic. 
-	</p> -->
 	<p>
-	<!-- I feel really lucky to have grown up around art & design, my dad & grandfathers are both designers. --> I am a designer, illustrator & painter. I am currently the Principal Designer at <a href="http://www.inflexxion.com/">Inflexxion, Inc.</a> but I also work on freelance & personal side projects. I am the father of 2 fantastic boys, and husband to an amazing wife. Coffee lover,  I often wish there were more hours in the day so I could spend more time doing what I love.
+	I am a designer, illustrator & painter. I am currently the Principal Designer at <a href="http://www.inflexxion.com/">Inflexxion, Inc.</a> but I also work on freelance & personal side projects. Father, coffee lover, tattoo collector, loud music blaster.
 	</p>
-
-	<!-- <p class="rounded">Full width bar</p> -->
 </div>
 
 <?php include ('sidebarabout.php'); ?>
